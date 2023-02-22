9aa6cf6dbeed875b82efcfec6e915088a7c13b0b
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-02-22 15:45:34 -0500

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
+import EyesOnMe from "./EyesOnMe";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function enter() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input onChange={enter} type="password" />
+      <EyesOnMe />
+    </div>
+  );
 }
 
 export default Keypad;
